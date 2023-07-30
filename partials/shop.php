de4cbb343ff3be95a5843b225a99c672ec1f9a33
webozza <webozza@gmail.com>
2023-07-30 11:34:40 +0800

    version update

--- partials/shop.php
+++ partials/shop.php
@@ -10,9 +10,11 @@
 			<h2>Choose your Domain Type</h2>
 			<div class="domain-inventory-domain-type switch-domain-type">
 				<input type="radio" id="domain-type-budget" name="domain-type" value="Budget">
-				<label for="domain-type-budget" class="show-tooltip" title="Budget domains are suitable for PBN, 301 Redirection and Money Site">Budget Domains</label>
+				<label for="domain-type-budget" class="show-tooltip">Budget Domains</label>
 				<input type="radio" id="domain-type-premium" name="domain-type" value="Premium">
-				<label for="domain-type-premium" class="show-tooltip" title="Premium Domains are super powerful with high authority backlinks that will provide faster and better results.">Premium Domains</label>
+				<label for="domain-type-premium" class="show-tooltip">Premium Domains</label>
+                <input type="radio" id="domain-type-30" name="domain-type" value="30">
+				<label for="domain-type-30" class="show-tooltip">$30 Domains</label>
 			</div>
 			<div class="domain-inventory-search-field">
 				<input type="text" name="domain-search" id="domain-search" placeholder="Search Domain ID / Domain Name / Keyword / Niche" value="">
