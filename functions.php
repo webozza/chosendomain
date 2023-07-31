cf156eddcaf7b495242da7d622d76c28e192eea5
ashraful-webozza <ashraful@webozza.com>
2023-07-31 13:22:59 +0600

    nouislider

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.' );
  */
 function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/css/nouislider.css', array(), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+	wp_enqueue_style( 'nouislider', get_stylesheet_directory_uri() . '/css/nouislider.css', array(), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
