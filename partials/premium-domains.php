5e8e8416610cc6ae8228b3b761684de6e7141c88
toriqul09 <toriqul@webozza.com>
2023-08-14 11:36:59 +0600

    domain upload

--- partials/premium-domains.php
+++ partials/premium-domains.php
@@ -41,27 +41,27 @@
         <div class="domain-inventory-search-box">
             <h2>Choose your Domain Type</h2>
             <div class="domain-inventory-domain-type switch-domain-type">
-                <input type="radio" id="domain-type-budget" name="domain-type[]" value="Budget">
-                <label for="domain-type-budget" class="show-tooltip">Budget Domains</label>
+                <!-- <input type="radio" id="domain-type-budget" name="domain-type[]" value="Budget">
+                <label for="domain-type-budget" class="show-tooltip">Budget Domains</label> -->
                 <input type="radio" id="domain-type-premium" name="domain-type[]" value="Premium">
                 <label for="domain-type-premium" class="show-tooltip">Premium Domains</label>
-                <input type="radio" id="domain-type-30" name="domain-type[]" value="30">
-                <label for="domain-type-30" class="show-tooltip">$30 Domains</label>
+                <!-- <input type="radio" id="domain-type-30" name="domain-type[]" value="30">
+                <label for="domain-type-30" class="show-tooltip">$30 Domains</label> -->
             </div>
-            <div style="display:none;" class="domain-inventory-search-field">
+            <!-- <div style="display:none;" class="domain-inventory-search-field">
                 <input type="text" name="domain-search" id="domain-search"
                     placeholder="Search Domain Name" value="">
                 <button class="fire-domain-keyword-search">SEARCH</button>
-            </div>
+            </div> -->
         </div>
 
         <!-- Domain Search Controls -->
-        <div class="domain-search-controls">
+        <!-- <div class="domain-search-controls">
             <button class="di-hide-filters dc-btn dc-btn-primary-muted dc-btn-big">
 				<span class="hide-on-mobile">Hide filters</span> 
 				<i class="far fa-eye-slash"></i>
 			</button>
-        </div>
+        </div> -->
 
         <!-- Domain Loop with Filters -->
         <div class="domain-inventory-search-wrap">
