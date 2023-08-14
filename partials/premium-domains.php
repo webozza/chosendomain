93eae22394a4bdbb5f6ece0cfe59efc4c8bbdd07
toriqul09 <toriqul@webozza.com>
2023-08-14 12:09:41 +0600

    premium domain

--- partials/premium-domains.php
+++ partials/premium-domains.php
@@ -582,8 +582,8 @@
 							// Domain Type
 							// $domain_type = get_field('domain_type', $product_id);
                             $domain_type = get_field('domain_type', $product_id);
-                            var_dump($domain_type);
-                            if($domain_type == "Premium Domain") {
+                            // var_dump($domain_type);
+                            if($domain_type == "[Premium Domain]") {
                                  $premiumDomain = get_field('domain_type', $product_id);
                                  var_dump($premiumDomain);
                             }
