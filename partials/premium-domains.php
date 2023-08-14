b8feb03c3ac3ce875f7f4446491d9122b4be0eb5
toriqul09 <toriqul@webozza.com>
2023-08-14 12:05:12 +0600

    checked domain for primium domain

--- partials/premium-domains.php
+++ partials/premium-domains.php
@@ -582,7 +582,7 @@
 							// Domain Type
 							// $domain_type = get_field('domain_type', $product_id);
                             $domain_type = get_field('domain_type', $product_id);
-
+                            var_dump($domain_type);
                             if($domain_type == "Premium Domain") {
                                  $premiumDomain = get_field('domain_type', $product_id);
                                  var_dump($premiumDomain);
