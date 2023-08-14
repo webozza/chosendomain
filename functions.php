ccbce84c9568f9ed1df74f9c894fe8abeb3491cc
webozza <webozza@gmail.com>
2023-08-14 12:54:09 +0800

    version update

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.1.01' );
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.1.02' );
 
 /**
  * Enqueue styles
---
+++ partials/premium-domains.php
@@ -0,0 +1,10 @@
+<?php
+    /* Template Name: Premium Domains */
+?>
+
+<?php get_header(); ?>
+
+<h1>Premium Domains</h1>
+
+<?php get_footer(); ?>
+
