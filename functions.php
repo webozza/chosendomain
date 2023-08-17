675b9ff2e82f77a5defc2d547c29d12806e04097
webozza <webozza@gmail.com>
2023-08-17 21:24:07 +0800

    version update

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.1.55' );
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.1.56' );
 
 /**
  * Enqueue styles
@@ -34,7 +34,7 @@ add_action( 'wp_enqueue_scripts', 'custom_scripts');
 add_action('wp_ajax_load_more_posts', 'load_more_posts');
 add_action('wp_ajax_nopriv_load_more_posts', 'load_more_posts');
 
-function load_more_products() {
+function load_more_posts() {
     $page = $_POST['page'];
     $posts_per_page = 10; // Number of products per page
 
