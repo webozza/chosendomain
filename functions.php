6a0e04c1c561d5b18c16aadc7e772d86533ebd2b
webozza <webozza@gmail.com>
2023-08-20 14:02:36 +0800

    version update

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.2.39' );
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.2.40' );
 
 // Enable error reporting and display errors for debugging
 error_reporting(E_ALL);
--- partials/premium-domains.php
+++ partials/premium-domains.php
@@ -8,10 +8,22 @@
 
 <!-- ------------------ start ----------- -->
 <?php
+	$domainType = 'Premium Domain';
+	$current_page = max(1, get_query_var('paged'));
+	$posts_per_page = 5;
+	
     $args = array(
-        'post_type' => 'product',
-        'posts_per_page' => -1,
-    );
+		'post_type' => 'product',
+		'posts_per_page' => -1,
+		'paged' => $current_page,
+		'meta_query' => array(
+			array(
+				'key' => 'domain_type',
+				'value' => $domainType,
+				'compare' => '=',
+			),
+		),
+	);	
     $product_query = new WC_Product_Query($args);
     $premium_products = $product_query->get_products();
 	$product_cats = get_terms(array(
@@ -85,8 +97,8 @@
             <!-- DOMAINS -->
             <div class="domain-inventory-content" id="product-container">
                 <?php
-					if ($premium_products) {
-						foreach ($premium_products as $product) {
+					if ($product_query->have_posts()) {
+						while ($product_query->have_posts()) {
 							
 							//$product_id = $product->get_id();
 							$product_id = $product->get_id();
@@ -136,52 +148,75 @@
 								$uses = $use_cases[0];
 							}
 					?>
-				<?php if($domain_type == 'Premium Domain') { ?>
-					<div class="product-box visible" data-domain-name="<?= $product_title ?>" data-domain-extension='<?= esc_attr(json_encode($extension_names)); ?>' data-domain-type="<?= $domain_type ?>" data-auth-backlinks='<?= json_encode($ab_names) ?>' data-languages='<?= json_encode($langs) ?>' data-use-cases='<?= json_encode($uses) ?>'> 
-						<div class="product-details">
-							<div class="product-head">
-								<div class="product-img">
-									<?php if ($product_image_url) { ?>
-										<img src="<?= $product_image_url ?>" alt="product image">
-									<?php } else { ?>
-										<img src="<?= get_site_url() . '/wp-content/uploads/woocommerce-placeholder.png' ?>" alt="product image">
-									<?php } ?>
-								</div>
-								<div class="product-title"> 
-									<label>
-										<span class="obscured-domain-name"> <?= $product_title ?> </span> 
-									</label> 
-									<br>
-									<div class="description hidden">
-										<a href="javascript:void(0)"> <img src="/wp-content/uploads/2023/08/heart-love.jpg"> </a>
+						<?php if($domain_type == 'Premium Domain') { ?>
+							<div class="product-box visible" data-domain-name="<?= $product_title ?>" data-domain-extension='<?= esc_attr(json_encode($extension_names)); ?>' data-domain-type="<?= $domain_type ?>" data-auth-backlinks='<?= json_encode($ab_names) ?>' data-languages='<?= json_encode($langs) ?>' data-use-cases='<?= json_encode($uses) ?>'> 
+								<div class="product-details">
+									<div class="product-head">
+										<div class="product-img">
+											<?php if ($product_image_url) { ?>
+												<img src="<?= $product_image_url ?>" alt="product image">
+											<?php } else { ?>
+												<img src="<?= get_site_url() . '/wp-content/uploads/woocommerce-placeholder.png' ?>" alt="product image">
+											<?php } ?>
+										</div>
+										<div class="product-title"> 
+											<label>
+												<span class="obscured-domain-name"> <?= $product_title ?> </span> 
+											</label> 
+											<br>
+											<div class="description hidden">
+												<a href="javascript:void(0)"> <img src="/wp-content/uploads/2023/08/heart-love.jpg"> </a>
+											</div>
+											<!--<div class="domain-name-revealer">
+												<i class="flaticon-eye"></i>
+											</div>-->
+										</div>
+										<h6>$<?= $price ?></h6>
+										<div class="product-card">
+											<ul>
+												<li>
+													<a href="?add-to-cart=<?= $product_id ?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart " data-product_id="<?= $product_id ?>" data-product_sku="" aria-label="Add “<?= $product_title ?>” to your cart" aria-describedby="" rel="nofollow">Add to cart</a>
+												</li>
+												<li> <a href="<?= get_site_url() . '/product/' . $product_slug ?>"> More Data </a> </li>
+											</ul>
+										</div>
 									</div>
-									<!--<div class="domain-name-revealer">
-										<i class="flaticon-eye"></i>
-									</div>-->
-								</div>
-								<h6>$<?= $price ?></h6>
-								<div class="product-card">
-									<ul>
-										<li>
-											<a href="?add-to-cart=<?= $product_id ?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart " data-product_id="<?= $product_id ?>" data-product_sku="" aria-label="Add “<?= $product_title ?>” to your cart" aria-describedby="" rel="nofollow">Add to cart</a>
-										</li>
-										<li> <a href="<?= get_site_url() . '/product/' . $product_slug ?>"> More Data </a> </li>
-									</ul>
+									<div class="product-body" style="display:none;">
+										<div class="catgories"> 
+											<?php foreach($product_categories as $catagory) { ?>
+												<span><?= $catagory?></span>
+											<?php }?>
+												<a class="hidden" href="<?= the_permalink($catagory_id -> ID);?>"> View Links </a> 
+										</div>
+									</div>
 								</div>
 							</div>
-							<div class="product-body" style="display:none;">
-								<div class="catgories"> 
-									<?php foreach($product_categories as $catagory) { ?>
-										<span><?= $catagory?></span>
-									<?php }?>
-										<a class="hidden" href="<?= the_permalink($catagory_id -> ID);?>"> View Links </a> 
+						<?php } ?>
+						<?php
+							$pagination_args = array(
+								'base' => esc_url(add_query_arg('paged', '%#%')),
+								'format' => '?paged=%#%',
+								'total' => $product_query->max_num_pages,
+								'current' => $current_page,
+								'prev_next' => true,
+								'prev_text' => __('&laquo; Previous'),
+								'next_text' => __('Next &raquo;'),
+							);
+
+							?>
+								<div class="pagination-section" id="">
+									<p class="hidden">Showing <?= $totalProducts; ?> domains filtered out of <?= $totalProducts; ?> domains</p>
+									<div id="pagination-container" class="pagination">
+										<?php
+										if ($pagination_links) {
+											echo $pagination_links;
+										}
+										?>
+									</div>
 								</div>
-							</div>
-						</div>
-					</div>
-				<?php } ?>
-                <?php
-						}
+							<?php
+
+								}
 					} else {
 						echo 'No products found.';
 					}
@@ -191,6 +226,77 @@
                     No results found to the selected filters. Please change/remove filters to show domains.
                 </div>
             </div>
+
+			<div class="domain-inventory-content" id="product-container">
+				<?php if ($product_query->have_posts()) {
+					while ($product_query->have_posts()) {
+						$product_id = get_the_ID(); // Use get_the_ID() to get post ID
+						$product_title = get_the_title();
+						$product_slug = $product->get_slug();
+						$price = $product -> get_price();
+						$da = get_post_meta($product_id, 'da', true);
+						$product_image_url = get_the_post_thumbnail_url($product_id, 'full');
+						$product_categories = wp_get_post_terms($product_id, 'product_cat', array('fields' => 'names'));
+
+						// Domain Type
+						$domain_type = get_field('domain_type', $product_id);
+
+						// Authority Backlinks
+						$authority_backlinks = wp_get_post_terms($product_id, 'authory_backlink');
+						$ab_names = array();
+						foreach ($authority_backlinks as $backlink) {
+							$ab_names[] = $backlink->name;
+						};
+
+						// Language
+						$languages  = wp_get_post_terms($product_id, 'language');
+						//var_dump($languages);
+						$langs = array();
+						foreach ($languages as $lang) {
+							$langs[] = $lang->name;
+						};
+
+						// Use Cases
+						$use_cases = get_post_meta($product_id, 'usecase', false);
+						if(empty($use_cases)) {
+							$uses = [];
+						} else {
+							$uses = $use_cases[0];
+						}
+						?>
+						<?php if ($domain_type == 'Premium Domain') { ?>
+							<div class="product-box visible" data-domain-name="<?= $product_title ?>" data-domain-type="<?= $domain_type ?>">
+								<div class="product-details">
+									<!-- Product details content -->
+								</div>
+							</div>
+						<?php } ?>
+					<?php }
+					
+					// Pagination
+					$pagination_args = array(
+						'base' => esc_url(add_query_arg('paged', '%#%')),
+						'format' => '?paged=%#%',
+						'total' => $product_query->max_num_pages,
+						'current' => $current_page,
+						'prev_next' => true,
+						'prev_text' => __('&laquo; Previous'),
+						'next_text' => __('Next &raquo;'),
+					);
+					?>
+					<div class="pagination-section" id="">
+						<p class="hidden">Showing <?= $totalProducts; ?> domains filtered out of <?= $totalProducts; ?> domains</p>
+						<div id="pagination-container" class="pagination">
+							<?php if ($pagination_links) {
+								echo $pagination_links;
+							} ?>
+						</div>
+					</div>
+				<?php } else {
+					echo 'No products found.';
+				} ?>
+			</div>
+
 			<!-- LOAD MORE SECTION -->
 			<div class="load-more-container">
 				<div id="loading-text" style="display: none;">
