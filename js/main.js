8403173c4266327bdb0428ffa3e91e93a2594879
webozza <webozza@gmail.com>
2024-03-29 19:32:31 +0600

    version update

--- js/main.js
+++ js/main.js
@@ -479,7 +479,7 @@ jQuery(document).ready(function ($) {
 
   //---------------- Category Filter ------------
   $(".google-index-filter.slide-accor select").change(function () {
-    applyFiltersWithAjax(searchTerm); // Call the combined filtering function
+    // applyFiltersWithAjax(searchTerm);
   });
 
   //---------------- Domain Extension Filter ------------
